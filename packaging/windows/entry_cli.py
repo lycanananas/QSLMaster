a809f39c7c348e85f15bc2402a9ca330e49db76a
Adrian Grzeca <adrian@grzeca.me>
2026-04-05 10:31:22 +0200

    Fixed Linux

--- packaging/windows/entry_cli.py
+++ packaging/windows/entry_cli.py
@@ -7,6 +7,7 @@ if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
 
 if __name__ == '__main__':
-    mp.freeze_support()
+    if sys.platform == 'win32':
+        mp.freeze_support()
     from qslmaster_cli.main import main
     main()
