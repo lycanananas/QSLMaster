5566479f6bc6faf06066533740fd04a6a0d53c96
Adrian "SQ5FOX" Grzeca <adrian@grzeca.me>
2026-04-05 10:13:20 +0200

    Release v1.5.2
    * Fixed Windows

--- packaging/windows/entry_gui.py
+++ packaging/windows/entry_gui.py
@@ -1,11 +1,12 @@
 import sys
+import multiprocessing as mp
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
 
-from qslmaster_gui.main import main
-
 if __name__ == '__main__':
+    mp.freeze_support()
+    from qslmaster_gui.main import main
     main()
